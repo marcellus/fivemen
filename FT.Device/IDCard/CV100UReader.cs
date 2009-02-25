f5a991ee22ede021eafd549a852f55fa50c77286
cbw123_1984 <cbw123_1984@163.com>
2009-02-25 15:39:35 +0000

    初始导入

---
+++ FT.Device/IDCard/CV100UReader.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing;
+
+namespace FT.Device.IDCard
+{
+    public class CV100UReader
+    {
+        #region dll导入
+        /*
+int CVR_InitComm(int Port)       						 初始化连接;
+int CVR_Authenticate()									 卡认证;
+int CVR_Read_Content(int Active)				    			 读卡操作。
+int CVR_CloseComm()			      						 关闭连接;
+int CVR_Ant(int mode)									 射频操作
+	int	CVR_ReadBaseMsg (
+		  unsigned char *pucCHMsg, unsigned int *puiCHMsgLen, 							  unsigned char *pucPHMsg, unsigned int *puiPHMsgLen, 
+		  int nMode)                                          读卡操作(读入内存)
+	int  GetPeopleName(char *strTmp, int strLen)	    			 	 得到姓名信息	
+	int  GetPeopleSex(char *strTmp, int strLen)	    			 		 得到性别信息	
+	int  GetPeopleNation(char *strTmp, int strLen)	    			 	 得到民族信息	
+	int  GetPeopleBirthday(char *strTmp, int strLen)			 		 得到出生日期	
+	int  GetPeopleAddress(char *strTmp, int strLen)			 		 得到地址信息	
+	int  GetPeopleIDCode(char *strTmp, int strLen)			 		 得到身份证号信息
+	int  GetDepartment(char *strTmp, int strLen)	    			 	 得到发证机关信息	
+	int  GetStartDate(char *strTmp, int strLen)	    					得到有效开始日期	
+	int  GetEndDate(char *strTmp, int strLen)	        				得到有效截止日期
+
+         */
+        #region 调用 CVR_Read_Content 或者 CVR_ReadBaseMsg 函数			成功后再分别调用以下函数
+        /// <summary>
+        ///得到姓名信息
+        /// </summary>
+        /// <param name="name">姓名</param>
+        /// <param name="namelen">字符串长度</param>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleName(StringBuilder name,int namelen);
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleSex(StringBuilder sex, int sexlen);
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleNation(StringBuilder nation, int nationlen);
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleBirthday(StringBuilder birthday, int birlen);
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleAddress(StringBuilder address, int addresslen);
+        [DllImport("termb.dll")]
+        public static extern int GetPeopleIDCode(StringBuilder idcode, int idcodelen);
+        [DllImport("termb.dll")]
+        public static extern int GetDepartment(StringBuilder dept, int deptlen);
+        [DllImport("termb.dll")]
+        public static extern int GetStartDate(StringBuilder start, int startlen);
+        [DllImport("termb.dll")]
+        public static extern int GetEndDate(StringBuilder end, int endlen);
+        #endregion
+
+        /// <summary>
+        /// 读文字、照片信息到自定义内存缓冲
+        /// </summary>
+        /// <param name="baseData">身份文字信息内存缓冲指针 out</param>
+        /// <param name="baseDataLen">身份文字信息长度默认 256 Byte</param>
+        /// <param name="picData">身份照片信息内存缓冲指针 out</param>
+        /// <param name="picDataLen">身份照片信息长度默认 1024 Byte</param>
+        /// <param name="mode">
+        /// 1、文字编码为默认UCS-2格式，照片未解压成bmp文件
+        /// 2、文字编码已转换成GBK国标码格式，照片未解压成bmp文件
+        /// 3、文字编码为默认UCS-2格式，照片已解压成zp.bmp文件
+        /// 4、文字编码已转换成GBK国标码格式，照片已解压成zp.bmp文件
+        /// </param>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll", CharSet = CharSet.Auto)]
+        public static extern int CVR_ReadBaseMsg(StringBuilder baseData,int baseDataLen,StringBuilder picData,int picDataLen,int mode);
+
+        /// <summary>
+        /// 本函数用于打开/关闭射频。阅读器在不读卡时，如果射频对其它的电子产品有干扰的话，
+        /// 可以选择关闭射频，当需要读卡时，再打开射频。该函数只在当连接到串行接口的阅读器时，
+        /// 调用有效。如不调用此函数时，射频一直处于打开状态。
+        /// 注：当 CVR_InitComm函数调用成功后，该函数有效。
+        /// </summary>
+        /// <param name="mode">
+        /// 1、打开射频
+        /// 0、关闭射频
+        /// </param>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll")]
+        public static extern int CVR_Ant(int mode);
+
+        /// <summary>
+        /// 初始化连接
+        /// </summary>
+        /// <param name="port">端口，1-4串口，1001-1004USB口</param>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll")]
+        public static extern int CVR_InitComm(int port);
+
+        /// <summary>
+        /// 关闭PC到阅读器的连接
+        /// </summary>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll")]
+        public static extern int CVR_CloseComm();
+
+
+        /// <summary>
+        /// 卡认证.
+        /// </summary>
+        /// <returns>注意：若卡片放置后发生认证错误时，应移走卡片重新放置。
+        /// 1、卡片正确放置时
+        /// 0、未放卡或卡片放置不正确时
+        /// </returns>
+        [DllImport("termb.dll")]
+        public static extern int CVR_Authenticate();
+
+        /// <summary>
+        ///读取身份证卡片内容
+        /// </summary>
+        /// <param name="active">
+        /// 1、生成文字WZ.TXT、相片数据XP.WLT和相片ZP.BMP(解码)
+        /// 2、生成文字WZ.TXT和相片数据XP.WLT
+        /// 3、生成最新住址NEWADD.TXT（卡无最新地址则生成空文件）
+        /// 4、生成WZ.TXT(解码)，相片ZP.BMP(解码)
+        /// 5、芯片管理号IINSNDN.bin
+        /// 6、以设备唯一标志号，生成文字WZ.TXT(解码)，相片XP.BMP(解码)（用于终端网络环境）
+        /// </param>
+        /// <returns>1正确0错误</returns>
+        [DllImport("termb.dll")]
+        public static extern int CVR_Read_Content(int active);
+        #endregion
+
+        public CV100UReader()
+        {
+        }
+
+        public delegate void ReadICCardComplete(IDCard card);
+        public event ReadICCardComplete AfterReadICCardComplete;
+
+        public int Init()
+        {
+            int usePort = -1;
+            //检测usb口的机具连接，必须先检测usb
+            for (int port = 1001; port <= 1016; port++)
+            {
+                int result = CVR_InitComm(port);
+                if (result == 1)
+                {
+                    usePort = port;
+                    break;
+                }
+            }
+            if (usePort == -1)
+            {
+                throw new Exception("初始化二代身份证读卡器出错！");
+                return -1;
+            }
+            if (CVR_Authenticate() == 0)
+            {
+                throw new Exception("认证二代身份证读卡器出错！");
+                return -1;
+            }
+            hasInit = true;
+            return usePort;
+        }
+
+        public void Close()
+        {
+            if (CVR_CloseComm() == 0)
+            {
+                throw new Exception("关闭卡出错！");
+            }
+        }
+
+        private bool hasInit = false;
+
+        /// <summary>
+        /// 读取身份证信息
+        /// </summary>
+        public void Read()
+        {
+            if (!this.hasInit)
+            {
+                this.Init();
+            }
+            IDCard card = new IDCard();
+            if (CVR_Read_Content(1) == 0)
+            {
+                throw new Exception("未放卡或卡片放置不正确！");
+            }
+
+        }
+
+        public void ReadFromFile(IDCard card)
+        {
+            FileInfo f = new FileInfo("wz.txt");
+            FileStream fs = f.OpenRead();
+            byte[] bt = new byte[fs.Length];
+            fs.Read(bt, 0, (int)fs.Length);
+            fs.Close();
+
+            string str = System.Text.UnicodeEncoding.Unicode.GetString(bt);
+
+            card.Name = System.Text.UnicodeEncoding.Unicode.GetString(bt, 0, 30).Trim();
+            card.Sex_Code = System.Text.UnicodeEncoding.Unicode.GetString(bt, 30, 2).Trim();
+            card.NATION_Code = System.Text.UnicodeEncoding.Unicode.GetString(bt, 32, 4).Trim();
+            string strBird = System.Text.UnicodeEncoding.Unicode.GetString(bt, 36, 16).Trim();
+            card.BIRTH = Convert.ToDateTime(strBird.Substring(0, 4) + "年" + strBird.Substring(4, 2) + "月" + strBird.Substring(6) + "日");
+            card.ADDRESS = System.Text.UnicodeEncoding.Unicode.GetString(bt, 52, 70).Trim();
+            card.IDC = System.Text.UnicodeEncoding.Unicode.GetString(bt, 122, 36).Trim();
+            card.REGORG = System.Text.UnicodeEncoding.Unicode.GetString(bt, 158, 30).Trim();
+            string strTem = System.Text.UnicodeEncoding.Unicode.GetString(bt, 188, bt.GetLength(0) - 188).Trim();
+            card.STARTDATE = Convert.ToDateTime(strTem.Substring(0, 4) + "年" + strTem.Substring(4, 2) + "月" + strTem.Substring(6, 2) + "日");
+            strTem = strTem.Substring(8);
+            if (strTem.Trim() != "长期")
+            {
+                card.ENDDATE = Convert.ToDateTime(strTem.Substring(0, 4) + "年" + strTem.Substring(4, 2) + "月" + strTem.Substring(6, 2) + "日");
+            }
+            else
+            {
+                card.ENDDATE = DateTime.MaxValue;
+            }
+            FileInfo objFile = null;
+            objFile = new FileInfo("zp.bmp");
+            if (objFile.Exists)
+            {
+                Image img = Image.FromFile("zp.bmp");
+                card.PIC_Image = (Image)img.Clone();
+                System.IO.MemoryStream m = new MemoryStream();
+                img.Save(m, System.Drawing.Imaging.ImageFormat.Jpeg);
+                card.PIC_Byte = m.ToArray();
+                img.Dispose();
+                img = null;
+            }
+        }
+
+    }
+}
---
+++ FT.Device/IDCard/IDCard.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+
+namespace FT.Device.IDCard
+{
+    /// <summary>
+    /// 二代证的信息类
+    /// </summary>
+    public class IDCard
+    {
+        private SortedList lstMZ = new SortedList();
+        private string _Name;   //姓名
+        private string _Sex_Code;   //性别代码
+        private string _Sex_CName;   //性别
+        private string _IDC;      //身份证号码
+        private string _NATION_Code;   //民族代码
+        private string _NATION_CName;   //民族
+        private DateTime _BIRTH;     //出生日期
+        private string _ADDRESS;    //住址
+        private string _REGORG;     //签发机关
+        private DateTime _STARTDATE;    //身份证有效起始日期
+        private DateTime _ENDDATE;    //身份证有效截至日期
+        private string _Period_Of_Validity_Code;   //有效期限代码，许多原来系统上面为了一代证考虑，常常存在这样的字段，二代证中已经没有了
+        private string _Period_Of_Validity_CName;   //有效期限
+        private byte[] _PIC_Byte;    //照片二进制
+        private Image _PIC_Image;   //照片
+
+        public IDCard()
+        {
+            lstMZ.Add("01", "汉族");
+            lstMZ.Add("02", "蒙古族");
+            lstMZ.Add("03", "回族");
+            lstMZ.Add("04", "藏族");
+            lstMZ.Add("05", "维吾尔族");
+            lstMZ.Add("06", "苗族");
+            lstMZ.Add("07", "彝族");
+            lstMZ.Add("08", "壮族");
+            lstMZ.Add("09", "布依族");
+            lstMZ.Add("10", "朝鲜族");
+            lstMZ.Add("11", "满族");
+            lstMZ.Add("12", "侗族");
+            lstMZ.Add("13", "瑶族");
+            lstMZ.Add("14", "白族");
+            lstMZ.Add("15", "土家族");
+            lstMZ.Add("16", "哈尼族");
+            lstMZ.Add("17", "哈萨克族");
+            lstMZ.Add("18", "傣族");
+            lstMZ.Add("19", "黎族");
+            lstMZ.Add("20", "傈僳族");
+            lstMZ.Add("21", "佤族");
+            lstMZ.Add("22", "畲族");
+            lstMZ.Add("23", "高山族");
+            lstMZ.Add("24", "拉祜族");
+            lstMZ.Add("25", "水族");
+            lstMZ.Add("26", "东乡族");
+            lstMZ.Add("27", "纳西族");
+            lstMZ.Add("28", "景颇族");
+            lstMZ.Add("29", "柯尔克孜族");
+            lstMZ.Add("30", "土族");
+            lstMZ.Add("31", "达翰尔族");
+            lstMZ.Add("32", "仫佬族");
+            lstMZ.Add("33", "羌族");
+            lstMZ.Add("34", "布朗族");
+            lstMZ.Add("35", "撒拉族");
+            lstMZ.Add("36", "毛南族");
+            lstMZ.Add("37", "仡佬族");
+            lstMZ.Add("38", "锡伯族");
+            lstMZ.Add("39", "阿昌族");
+            lstMZ.Add("40", "普米族");
+            lstMZ.Add("41", "塔吉克族");
+            lstMZ.Add("42", "怒族");
+            lstMZ.Add("43", "乌孜别克族");
+            lstMZ.Add("44", "俄罗斯族");
+            lstMZ.Add("45", "鄂温克族");
+            lstMZ.Add("46", "德昂族");
+            lstMZ.Add("47", "保安族");
+            lstMZ.Add("48", "裕固族");
+            lstMZ.Add("49", "京族");
+            lstMZ.Add("50", "塔塔尔族");
+            lstMZ.Add("51", "独龙族");
+            lstMZ.Add("52", "鄂伦春族");
+            lstMZ.Add("53", "赫哲族");
+            lstMZ.Add("54", "门巴族");
+            lstMZ.Add("55", "珞巴族");
+            lstMZ.Add("56", "基诺族");
+            lstMZ.Add("57", "其它");
+            lstMZ.Add("98", "外国人入籍");
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public string Sex_Code
+        {
+            get { return _Sex_Code; }
+            set
+            {
+                _Sex_Code = value;
+                switch (value)
+                {
+                    case "1":
+                        Sex_CName = "男";
+                        break;
+                    case "2":
+                        Sex_CName = "女";
+                        break;
+                }
+            }
+        }
+        public string Sex_CName
+        {
+            get { return _Sex_CName; }
+            set { _Sex_CName = value; }
+        }
+        public string IDC
+        {
+            get { return _IDC; }
+            set { _IDC = value; }
+        }
+        public string NATION_Code
+        {
+            get { return _NATION_Code; }
+            set
+            {
+                _NATION_Code = value;
+                if (lstMZ.Contains(value))
+                    NATION_CName = lstMZ[value].ToString();
+            }
+        }
+        public string NATION_CName
+        {
+            get { return _NATION_CName; }
+            set { _NATION_CName = value; }
+        }
+        public DateTime BIRTH
+        {
+            get { return _BIRTH; }
+            set { _BIRTH = value; }
+        }
+        public string ADDRESS
+        {
+            get { return _ADDRESS; }
+            set { _ADDRESS = value; }
+        }
+        public string REGORG
+        {
+            get { return _REGORG; }
+            set { _REGORG = value; }
+        }
+        public DateTime STARTDATE
+        {
+            get { return _STARTDATE; }
+            set { _STARTDATE = value; }
+        }
+        public DateTime ENDDATE
+        {
+            get { return _ENDDATE; }
+            set
+            {
+                _ENDDATE = value;
+                if (_ENDDATE == DateTime.MaxValue)
+                {
+                    _Period_Of_Validity_Code = "3";
+                    _Period_Of_Validity_CName = "长期";
+                }
+                else
+                {
+                    if (_STARTDATE != DateTime.MinValue)
+                    {
+                        switch (value.AddDays(1).Year - _STARTDATE.Year)
+                        {
+                            case 5:
+                                _Period_Of_Validity_Code = "4";
+                                _Period_Of_Validity_CName = "5年";
+                                break;
+                            case 10:
+                                _Period_Of_Validity_Code = "1";
+                                _Period_Of_Validity_CName = "10年";
+                                break;
+                            case 20:
+                                _Period_Of_Validity_Code = "2";
+                                _Period_Of_Validity_CName = "20年";
+                                break;
+                        }
+                    }
+                }
+
+            }
+        }
+        public string Period_Of_Validity_Code
+        {
+            get { return _Period_Of_Validity_Code; }
+            set { _Period_Of_Validity_Code = value; }
+        }
+        public string Period_Of_Validity_CName
+        {
+            get { return _Period_Of_Validity_CName; }
+            set { _Period_Of_Validity_CName = value; }
+        }
+        public byte[] PIC_Byte
+        {
+            get { return _PIC_Byte; }
+            set { _PIC_Byte = value; }
+        }
+        public Image PIC_Image
+        {
+            get { return _PIC_Image; }
+            set { _PIC_Image = value; }
+        }
+
+
+
+    }
+
+}
