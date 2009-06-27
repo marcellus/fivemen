0eab7af10ed04f75312f18edd43baf430ec3c245
cbw123_1984 <cbw123_1984@163.com>
2009-06-27 12:10:15 +0000

    加入统计功能完成

--- Jxc.Plugin/SellCounter.Designer.cs
+++ Jxc.Plugin/SellCounter.Designer.cs
@@ -31,28 +31,42 @@ namespace Jxc.Plugin
             this.btnDetail = new System.Windows.Forms.Button();
             this.dateBetweenPanel1 = new FT.Windows.Forms.CommonPanel.DateBetweenPanel();
             this.checkGroupTypeName = new System.Windows.Forms.CheckBox();
+            this.checkFrom = new System.Windows.Forms.CheckBox();
+            this.btnInDetail = new System.Windows.Forms.Button();
             this.groupSearch.SuspendLayout();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
+            // groupSearch
+            // 
+            this.groupSearch.Size = new System.Drawing.Size(898, 160);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
             // splitContainer2
             // 
             // 
             // splitContainer2.Panel1
             // 
+            this.splitContainer2.Panel1.Controls.Add(this.checkFrom);
             this.splitContainer2.Panel1.Controls.Add(this.checkGroupTypeName);
+            this.splitContainer2.Panel1.Controls.Add(this.btnInDetail);
             this.splitContainer2.Panel1.Controls.Add(this.btnDetail);
             this.splitContainer2.Panel1.Controls.Add(this.dateBetweenPanel1);
+            this.splitContainer2.Size = new System.Drawing.Size(892, 137);
+            this.splitContainer2.SplitterDistance = 677;
             // 
             // btnDetail
             // 
             this.btnDetail.Location = new System.Drawing.Point(382, 65);
             this.btnDetail.Name = "btnDetail";
-            this.btnDetail.Size = new System.Drawing.Size(75, 23);
+            this.btnDetail.Size = new System.Drawing.Size(116, 23);
             this.btnDetail.TabIndex = 6;
-            this.btnDetail.Text = "详细";
+            this.btnDetail.Text = "详细销售记录";
             this.btnDetail.UseVisualStyleBackColor = true;
             this.btnDetail.Click += new System.EventHandler(this.btnDetail_Click);
             // 
@@ -74,11 +88,34 @@ namespace Jxc.Plugin
             this.checkGroupTypeName.TabIndex = 7;
             this.checkGroupTypeName.Text = "是否分产品类别";
             this.checkGroupTypeName.UseVisualStyleBackColor = true;
+            this.checkGroupTypeName.Visible = false;
+            // 
+            // checkFrom
+            // 
+            this.checkFrom.AutoSize = true;
+            this.checkFrom.Location = new System.Drawing.Point(382, 39);
+            this.checkFrom.Name = "checkFrom";
+            this.checkFrom.Size = new System.Drawing.Size(101, 19);
+            this.checkFrom.TabIndex = 8;
+            this.checkFrom.Text = "是否分货源";
+            this.checkFrom.UseVisualStyleBackColor = true;
+            this.checkFrom.Visible = false;
+            // 
+            // btnInDetail
+            // 
+            this.btnInDetail.Location = new System.Drawing.Point(525, 65);
+            this.btnInDetail.Name = "btnInDetail";
+            this.btnInDetail.Size = new System.Drawing.Size(116, 23);
+            this.btnInDetail.TabIndex = 6;
+            this.btnInDetail.Text = "详细入库记录";
+            this.btnInDetail.UseVisualStyleBackColor = true;
+            this.btnInDetail.Click += new System.EventHandler(this.btnInDetail_Click);
             // 
             // SellCounter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.Name = "SellCounter";
+            this.Size = new System.Drawing.Size(898, 556);
             this.groupSearch.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
             this.splitContainer2.Panel1.PerformLayout();
@@ -93,5 +130,7 @@ namespace Jxc.Plugin
         private System.Windows.Forms.Button btnDetail;
         private FT.Windows.Forms.CommonPanel.DateBetweenPanel dateBetweenPanel1;
         private System.Windows.Forms.CheckBox checkGroupTypeName;
+        private System.Windows.Forms.CheckBox checkFrom;
+        private System.Windows.Forms.Button btnInDetail;
     }
 }
--- Jxc.Plugin/SellCounter.cs
+++ Jxc.Plugin/SellCounter.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using FT.DAL;
+using System.Collections;
+using FT.Windows.CommonsPlugin;
 
 namespace Jxc.Plugin
 {
@@ -14,57 +16,173 @@ namespace Jxc.Plugin
         public SellCounter()
         {
             InitializeComponent();
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.ClearColumns();
-            this.CreateColumn("时间",120);
-            this.CreateColumn("进库", 120);
-            this.CreateColumn("出库", 120);
+           // this.dataGridView1.AutoGenerateColumns = false;
+            //this.ClearColumns();
+            //this.CreateColumn("日期", 120);
+            //this.CreateColumn("货品名称", 120);
+            //this.CreateColumn("货品来源", 120);
+            //this.CreateColumn("入库量", 120);
+            //this.CreateColumn("出库量", 120);
             //this.CreateColumn("金额").DefaultCellStyle.Format = "N2";
         }
 
         protected override void Search()
         {
-            /*string sql = "select c_feetype as 费用类别,sum(i_fee) as 金额";
+            /*SELECT format(a.date_in,"yyyy-MM-dd") AS 日期, a.c_name AS 货品名称, a.c_from AS 货品来源, IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu)) AS 入库, IIf(IsNull(sum(b.m_dun_shu)),0,sum(b.m_dun_shu)) AS 出库
+FROM table_jxc_in_record AS a LEFT JOIN table_jxc_sell_record AS b ON format(a.date_in,"yyyy-MM-dd")=format(b.date_sell,"yyyy-MM-dd")
+GROUP BY format(a.date_in,"yyyy-MM-dd"), a.c_name, a.c_from;
+*/
            
+            DataTable dt = new DataTable();
+            dt.Columns.Add("日期");
+            //if(this.checkGroupTypeName)
+            ArrayList typelist = FT.DAL.Orm.SimpleOrmOperator.QueryListAll(typeof(BaseData));
+            BaseData type = null;
+            ArrayList fromlist = JxcHelper.GetFromList();
+            Dict from = null;
+            for (int i = 0; i < typelist.Count;i++ )
+            {
+                type = typelist[i] as BaseData;
+                dt.Columns.Add(type.TypeName + "入库量");
+                for (int j = 0; j < fromlist.Count; j++)
+                {
+                    from = fromlist[j] as Dict;
+                    dt.Columns.Add(type.TypeName+"("+from.Text + ")入库量");
+                    //dt.Columns.Add(type.TypeName+"("+from.Text + ")入库量");
+                }
 
-            IDataAccess access = FT.DAL.DataAccessFactory.GetDataAccess();
-            sql += " from table_car_fee where c_hmhp like '%"+
-                this.txtHmhp.Text.Trim()+"%' and " + access.BetweenDateString("date_fee", this.dateBetweenPanel1.BeginDate,
-                this.dateBetweenPanel1.EndDate);
-            sql += " group by c_feetype";
+                dt.Columns.Add(type.TypeName + "出库量");
+            }
+            dt.Columns.Add("本日入库量");
+            dt.Columns.Add("本日出库量");
+
+            string sql = "select format(a.date_in,\"yyyy-MM-dd\") AS 日期,";
             
-            DataTable dt = access
-            .SelectDataTable(sql, "tmp");
+            
+            IDataAccess access = FT.DAL.DataAccessFactory.GetDataAccess();
+            DataRow dr=null;
+            DateTime begindate1=this.dateBetweenPanel1.BeginDate;
+            DateTime begindate = begindate1;
+            
+            TimeSpan ts=this.dateBetweenPanel1.EndDate.Subtract(begindate);
+            int days=ts.Days+1;
+            DataTable tmptable=null;
+            for(int i=0;i<days;i++)
+            {
+                begindate=begindate1.AddDays(i);
+#region 分类分来源入库量
+                sql="SELECT c_name, c_from, format(a.date_in,\"yyyy-MM-dd\"), IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu))"
++" FROM table_jxc_in_record AS a "
++ " where format(a.date_in,\"yyyy-MM-dd\")='" + begindate.ToString("yyyy-MM-dd") + "' GROUP BY c_name, c_from, format(a.date_in,\"yyyy-MM-dd\")";
+
+                dr=dt.NewRow();
+                dr[0]=begindate.ToString("yyyy-MM-dd");
+                for (int j=1;j<dt.Columns.Count;j++)
+                {
+                    dr[j]=0;
+                }
+                tmptable=access.SelectDataTable(sql,"tmptable");
+                if(tmptable!=null)
+                {
+                     for (int k=0;k<tmptable.Rows.Count;k++)
+                    {
+                        dr[tmptable.Rows[k][0].ToString()+"("+tmptable.Rows[k][1].ToString()+")入库量"]=
+                            tmptable.Rows[k][3].ToString();
+                    }
+                }
+#endregion
+#region 分类出库量
+                sql="SELECT c_name,  format(a.date_sell,\"yyyy-MM-dd\"), IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu))"
++" FROM table_jxc_sell_record AS a "
++ " where format(a.date_sell,\"yyyy-MM-dd\")='" + begindate.ToString("yyyy-MM-dd") + "' GROUP BY c_name, format(a.date_sell,\"yyyy-MM-dd\")";
+                tmptable=access.SelectDataTable(sql,"tmptable");
+                if(tmptable!=null)
+                {
+                     for (int k=0;k<tmptable.Rows.Count;k++)
+                    {
+                        dr[tmptable.Rows[k][0].ToString()+"出库量"]=
+                            tmptable.Rows[k][2].ToString();
+                    }
+                }
+#endregion
+#region 分类不分来源入库量
+
+                    sql="SELECT c_name, format(a.date_in,\"yyyy-MM-dd\"), IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu))"
++" FROM table_jxc_in_record AS a "
++ " where format(a.date_in,\"yyyy-MM-dd\")='" + begindate.ToString("yyyy-MM-dd") + "' GROUP BY c_name, format(a.date_in,\"yyyy-MM-dd\")";
+                tmptable=access.SelectDataTable(sql,"tmptable");
+                if(tmptable!=null)
+                {
+                     for (int k=0;k<tmptable.Rows.Count;k++)
+                    {
+                        dr[tmptable.Rows[k][0].ToString()+"入库量"]=
+                            tmptable.Rows[k][2].ToString();
+                    }
+                }
+#endregion
+
+#region 今日入库出库量
+                                    sql="SELECT  IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu))"
++" FROM table_jxc_in_record AS a "
++" where format(a.date_in,\"yyyy-MM-dd\")='"+begindate.ToString("yyyy-MM-dd")+"'";
+                tmptable=access.SelectDataTable(sql,"tmptable");
+                if(tmptable!=null)
+                {
+                    dr["本日入库量"]=tmptable.Rows[0][0].ToString();
+                }
+                 sql="SELECT  IIf(IsNull(sum(a.m_dun_shu)),0,sum(a.m_dun_shu))"
++" FROM table_jxc_sell_record AS a "
++" where format(a.date_sell,\"yyyy-MM-dd\")='"+begindate.ToString("yyyy-MM-dd")+"'";
+                tmptable=access.SelectDataTable(sql,"tmptable");
+                if(tmptable!=null)
+                {
+                    dr["本日出库量"]=tmptable.Rows[0][0].ToString();
+                }
+#endregion
+                dt.Rows.Add(dr);
+            }
             
             
             if (dt != null)
             {
-                int count = 0;
+                dr = dt.NewRow();
+                dr[0] = "合计";
+                decimal[] counters =new decimal[dt.Columns.Count-1];
                 for (int i = 0; i < dt.Rows.Count; i++)
                 {
+                    
                     try
                     {
-                        count += Convert.ToInt32(dt.Rows[i].ItemArray[1].ToString());
+                        for (int k=0;k<counters.Length;k++)
+                        {
+                            counters[k]+=Convert.ToDecimal(dt.Rows[i].ItemArray[k+1].ToString());
+                        }
+                        
                     }
                     catch
                     {
                     }
                 }
-                dt.Rows.Add(new object[] {"合计",count });
+                for (int k = 0; k < counters.Length; k++)
+                {
+                    dr[k + 1] = counters[k].ToString();
+                }
+                dt.Rows.Add(dr);
+                //dt.Rows.Add(new object[] { "合计", "合计", "合计", countin, countout });*/
                 this.dataGridView1.DataSource = dt;
-             }*/
+             }
         }
 
         protected override string GetTitle()
         {
             return this.dateBetweenPanel1.BeginDate.ToShortDateString() + "至"
-                + this.dateBetweenPanel1.EndDate.ToShortDateString() + "销售统计";
+                + this.dateBetweenPanel1.EndDate.ToShortDateString() + "统计";
         }
 
         private void btnDetail_Click(object sender, EventArgs e)
         {
             Form form = new Form();
-            form.Text = this.GetTitle() + "详细信息";
+            form.Text = this.GetTitle() + "销售详细信息";
             SellRecordSearch search = new SellRecordSearch();
             search.Dock = DockStyle.Fill;
             IDataAccess access = FT.DAL.DataAccessFactory.GetDataAccess();
@@ -74,6 +192,26 @@ namespace Jxc.Plugin
             form.Controls.Add(search);
             form.Show();
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInDetail_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = this.GetTitle() + "入库详细信息";
+            InRecordSearch search = new InRecordSearch();
+            search.Dock = DockStyle.Fill;
+            IDataAccess access = FT.DAL.DataAccessFactory.GetDataAccess();
+            string condition = access.BetweenDateString("date_in", this.dateBetweenPanel1.BeginDate,
+                this.dateBetweenPanel1.EndDate);
+            search.SetConditions(condition);
+            form.Controls.Add(search);
+            form.Show();
+        }
     }
 }
+
 
