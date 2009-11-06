949970eab58e7174ed1854c16cf4a8656c80831e
cbw123_1984 <cbw123_1984@163.com>
2009-11-06 09:11:13 +0000

    新增FT的数据库测试工具

---
+++ FtDbConnectionTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FtDbConnectionTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            FT.Windows.Forms.AppicationHelper.BeforeStart();
+            Application.Run(new Form1());
+        }
+    }
+}
