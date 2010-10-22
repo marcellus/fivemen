a3ab93cc2097d3e4089355e66976d5990928185b
cbw123_1984 <cbw123_1984@163.com>
2010-10-22 08:02:24 +0000

    增加了数据库操作和初始化的类以及相应要用的dll

---
+++ PDA/PDA/PDA/DataInit/CreateTablesForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace PDA.DataInit
+{
+    public partial class CreateTablesForm : Form
+    {
+        public CreateTablesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.ExecuteFiles("ddl.sql");
+        }
+
+        private void ExecuteFiles(string filename)
+        {
+
+            string[] sqls = this.ReadSqls(filename);
+            MessageBox.Show("获取了" + sqls.Length + "条语句！");
+            int i = SqliteDbFactory.GetSqliteDbOperator().BatchExecute(sqls);
+            MessageBox.Show("成功执行了" + i + "条语句！");
+        }
+
+        private String[] ReadSqls(string filename)
+        {
+            string result = String.Empty;
+            filename = Path.GetDirectoryName(Assembly.Load(Assembly.GetExecutingAssembly().GetName()).GetName().CodeBase) + "\\" + filename;
+            try
+            {
+                //读取INI文件；
+                System.IO.StreamReader iniFile = new System.IO.StreamReader(filename, System.Text.Encoding.Default);
+                result = iniFile.ReadToEnd();
+                iniFile.Close();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+                
+            }
+            return result.Split(';');
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.ExecuteFiles("drop-ddl.sql");
+            
+        }
+    }
+}
---
+++ PDA/PDA/PDA/DataInit/SqliteTestForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace PDA.DataInit
+{
+    public partial class SqliteTestForm : Form
+    {
+        public SqliteTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //SqliteDbFactory.GetSqliteDbOperator().CreateDb();
+            //MessageBox.Show("创建数据库成功！");
+            CreateTablesForm form = new CreateTablesForm();
+            form.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = SqliteDbFactory.GetSqliteDbOperator().ExecuteNonQuery("insert into dept(deptid,deptname) values(1,'name1')");
+            if(i>0)
+            {
+                MessageBox.Show("插入数据成功！"+i);
+
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataGridForm frm = new DataGridForm();
+            frm.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             SqliteDbFactory.GetSqliteDbOperator().CreateTable("create table dept(deptid integer,deptname varchar(30))");
+            MessageBox.Show("创建数据表成功！");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i =  SqliteDbFactory.GetSqliteDbOperator().ExecuteNonQuery("update dept set deptname=deptname+'modify' where deptid=1");
+            if (i > 0)
+            {
+                MessageBox.Show("修改数据成功！"+i);
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = SqliteDbFactory.GetSqliteDbOperator().ExecuteNonQuery("delete from dept where deptid=1");
+            if (i > 0)
+            {
+                MessageBox.Show("修改数据成功！"+i);
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+             SqliteDbFactory.GetSqliteDbOperator().DropTable("dept");
+            MessageBox.Show("Drop表成功！" );
+        }
+    }
+}
