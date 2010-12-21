23ff0b715f3333c58f931a594352181b2c08462b
cbw123_1984 <cbw123_1984@163.com>
2010-12-21 08:46:52 +0000

    增加计算百分比的工具

---
+++ FT.Commons/Tools/MathHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FT.Commons.Tools
+{
+    /// <summary>
+    /// 数字操作类
+    /// </summary>
+    public class MathHelper : BaseHelper
+    {
+        private MathHelper()
+        {
+        }
+
+        public static string Percent(double a,double b)
+        {
+            if (b == 0.0d)
+            {
+                return "0.00%";
+            }
+            double percent = Math.Round(a / b , 4);
+            return percent.ToString("p");
+
+        }
+    }
+}
