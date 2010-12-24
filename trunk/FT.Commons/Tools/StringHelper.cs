a44b3765f9d9c448d746ff0d8907ecde169834cc
smartken0824@gmail.com <smartken0824@gmail.com@862885c0-6149-0410-b969-135527a0a411>
2010-12-24 09:07:55 +0000

    字符处理类

---
+++ trunk/FT.Commons/Tools/StringHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FT.Commons.Tools
+{
+   public class StringHelper :BaseHelper
+    {
+
+        public static string fnFormatNullOrBlankString(String pStrCheck)
+        {
+           return  fnFormatNullOrBlankString(pStrCheck, "");
+        }
+
+        public static string fnFormatNullOrBlankString(string pStrCheck, string pStrDefault)
+        {
+            if (pStrCheck == null)
+                return pStrDefault;
+            else if (pStrCheck.Length <1)
+                return pStrDefault;
+            else
+                return pStrCheck;
+        }
+
+        public static int fnFormatNullOrBlankInt(string pStrCheck)
+        {
+          return  fnFormatNullOrBlankInt(pStrCheck, 0);
+        }
+
+        public static int fnFormatNullOrBlankInt(string pStrCheck, int pIntDefault)
+        {
+            try
+            {
+                if (pStrCheck == null)
+                    return pIntDefault;
+                else if (pStrCheck.Length <1)
+                    return pIntDefault;
+                else
+                    return int.Parse(pStrCheck);
+            }
+            catch (FormatException fe)
+            {
+                return pIntDefault;
+            }
+   
+        }
+
+ 
+
+    }
+}
