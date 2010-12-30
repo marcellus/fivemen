4840b704d66f97bbef12f7839f5716dfced9654a
cbw123_1984@163.com <cbw123_1984@163.com@862885c0-6149-0410-b969-135527a0a411>
2010-12-30 08:21:41 +0000

    添加车主信息备案类

---
+++ trunk/DrvHelperSystem/App_Code/DriverPerson/InfoChange/CarOwnerInfoChangeOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using FT.DAL.Orm;
+
+/// <summary>
+///CarOwnerInfoChangeOperator 的摘要说明
+/// </summary>
+public class CarOwnerInfoChangeOperator
+{
+    public CarOwnerInfoChangeOperator()
+    {
+        //
+        //TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    public static void Delete(int id)
+    {
+        SimpleOrmOperator.Delete<CarOwnerInfoChange>(id);
+    }
+
+}
