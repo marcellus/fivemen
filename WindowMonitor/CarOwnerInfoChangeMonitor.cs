24b5108cd2f801020af42fc115f602a6ac650a59
cbw123_1984 <cbw123_1984@163.com>
2010-12-30 08:24:05 +0000

    添加车主信息备案监控模块。需要待验证以及接口的接入

--- WindowMonitor/CarOwnerInfoChangeMonitor.cs
+++ WindowMonitor/CarOwnerInfoChangeMonitor.cs
@@ -2,35 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 using FT.Windows.Forms.Monitor;
+using System.Windows.Forms;
+using FT.Windows.Forms.Monitor;
 using FT.DAL;
 using System.Data;
 using FT.WebServiceInterface.WebService;
-using System.Windows.Forms;
 
 namespace WindowMonitor
 {
-    public class PersonChangeMonitor:BaseMonitor
+    public class CarOwnerInfoChangeMonitor : BaseMonitor
     {
         protected override void DoTask()
         {
-            string sql = "select idacard,c_new_phone,c_new_postcode,c_new_address,c_old_phone,c_email from table_person_change_info where i_syn=0";
-            DataTable dt = DataAccessFactory.GetDataAccess().SelectDataTable(sql,"tmpdb");
+            string sql = "select c_cjh,c_fdjh,c_djzs,c_hmhp,c_hpzl,c_old_phone,c_new_phone,c_new_address,c_new_postcode,c_email from table_person_change_info where i_syn=0";
+            DataTable dt = DataAccessFactory.GetDataAccess().SelectDataTable(sql, "tmpdb");
             string logstr = string.Empty;
             if (dt != null && dt.Rows.Count > 0)
             {
-                for(int i=0;i<dt.Rows.Count;i++)
+                for (int i = 0; i < dt.Rows.Count; i++)
                 {
                     DrvPresonInfoChangeRequest request = new DrvPresonInfoChangeRequest();
                     request.sfzmhm = dt.Rows[i][0] == null ? "" : dt.Rows[i][0].ToString();
                     request.lxdh = dt.Rows[i][1] == null ? "" : dt.Rows[i][1].ToString();
                     request.yzbm = dt.Rows[i][2] == null ? "" : dt.Rows[i][2].ToString();
-                    request.lxzs=dt.Rows[i][3] == null ? "" : dt.Rows[i][3].ToString();
+                    request.lxzs = dt.Rows[i][3] == null ? "" : dt.Rows[i][3].ToString();
                     request.sjhm = dt.Rows[i][4] == null ? "" : dt.Rows[i][4].ToString();
                     request.dzyx = dt.Rows[i][5] == null ? "" : dt.Rows[i][5].ToString();
                     logstr = "正在处理身份证号码为:" + request.sfzmhm + "的信息本案变更！";
                     this.SetHintText(logstr);
                     this.CreateLog(logstr);
-                    TmriResponse response= DriverInterface.WritePersonInfoChange(request);
+                    TmriResponse response = DriverInterface.WritePersonInfoChange(request);
                     if (response.Code == 0)
                     {
                         logstr = "成功处理身份证号码：" + request.sfzmhm;
@@ -48,7 +49,7 @@ namespace WindowMonitor
 
             }
         }
-        public PersonChangeMonitor(NotifyIcon icon, int interval, string monitor)
+        public CarOwnerInfoChangeMonitor(NotifyIcon icon, int interval, string monitor)
             : base(icon, interval, monitor)
         {
            
