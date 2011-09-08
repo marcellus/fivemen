a3f7645e78d858015b10fc897061e7b9c3e5b6de
smartken0824@gmail.com <smartken0824@gmail.com@862885c0-6149-0410-b969-135527a0a411>
2011-09-08 09:19:10 +0000

    修正

--- trunk/DrvHelperSystem/FpSystem/FpHelper/FpCheckinLogStatis.aspx.cs
+++ trunk/DrvHelperSystem/FpSystem/FpHelper/FpCheckinLogStatis.aspx.cs
@@ -16,7 +16,7 @@ public partial class FpSystem_FpHelper_FpCheckinLogStatis : System.Web.UI.Page
     }
     protected void btnQuery_Click(object sender, EventArgs e)
     {
-        string patternQuery = "select m.NAME as name,m.BUSTYPE as bustype,"
+        string patternQuery = "select m.NAME as name,m.BUSTYPE as bustype,decode(m.BUSTYPE,'lesson','上课','train','入场训练','km1','科目一考试','km2','科目二考试','km3','科目三考试','collect','指纹采集') as bustype_name, "
         + " (SELECT count(s.ID) from Fp_Checkin_Log s where s.site_id=m.id and (s.checkin_date between to_date('{0}','YYYY-MM-DD') and to_date('{1}','YYYY-MM-DD') )  ) as tnum"
         + " from FP_SITE m"
         ;
