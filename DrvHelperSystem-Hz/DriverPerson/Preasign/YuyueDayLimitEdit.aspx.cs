5657456160eeb29ba5ad23448041946c7b264aed
ioriandk <ioriandk@gmail.com>
2011-10-21 07:30:13 +0000

    增加了预约时间间隔模块编写

---
+++ DrvHelperSystem-Hz/DriverPerson/Preasign/YuyueDayLimitEdit.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DrvHelperSystemBLL.DriverPerson.Model;
+using FT.DAL.Orm;
+using FT.Commons.Tools;
+using FT.Web.Tools;
+
+public partial class DriverPerson_Preasign_YuyueDayLimitEdit : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Request.Params["id"] != null)
+            {
+                YuyueDayLimit entity = SimpleOrmOperator.Query<YuyueDayLimit>(Convert.ToInt32(Request.Params["id"]));
+                WebFormHelper.SetDataToForm(this, entity);
+            }
+        }
+    }
+    protected void btnSure_Click(object sender, EventArgs e)
+    {
+        YuyueDayLimit entity = new YuyueDayLimit();
+        WebFormHelper.GetDataFromForm(this, entity);
+        if (entity.Id < 0)
+        {
+            if (SimpleOrmOperator.Create(entity))
+            {
+                WebTools.Alert("添加成功！");
+            }
+            else
+            {
+                WebTools.Alert("添加失败！");
+            }
+        }
+        else {
+            if (SimpleOrmOperator.Update(entity))
+            {
+                WebTools.Alert("修改成功！");
+            }
+            else
+            {
+                WebTools.Alert("修改失败！");
+            }
+        }
+    }
+}
