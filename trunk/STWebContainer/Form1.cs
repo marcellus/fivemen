3acb0c7c4a770b7b53c77fc90006ccf6e5b41e51
cbw123_1984@163.com <cbw123_1984@163.com@862885c0-6149-0410-b969-135527a0a411>
2012-01-18 09:31:24 +0000

    增加定时关机和是否维护模式

--- trunk/STWebContainer/Form1.cs
+++ trunk/STWebContainer/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using FT.Commons.Cache;
+using System.Diagnostics;
 
 namespace STWebContainer
 {
@@ -49,9 +50,44 @@ namespace STWebContainer
         {
             this.WindowState = FormWindowState.Maximized;
             STWebContainerSetting set = StaticCacheManager.GetConfig<STWebContainerSetting>();
-            if(set.Url.Length>0)
+            if (set.Url.Length > 0)
+            {
                 this.webBrowser.Navigate(set.Url);
+            }
+            this.splitContainer2.Visible = set.IsShowToolBar;
+            if (!set.IsShowToolBar)
+            {
+                //this.splitContainer1.Panel2.Height = 0;
+                this.splitContainer1.FixedPanel = FixedPanel.None;
+                this.splitContainer1.SplitterDistance = this.Height;
+               // this.splitContainer1.Panel1.
+            }
+            if (set.IsTimer)
+            {
+                this.timer1.Start();
+            }
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            STWebContainerSetting set = StaticCacheManager.GetConfig<STWebContainerSetting>();
+            DateTime now=System.DateTime.Now;
+            if (now.Hour == set.Hour && now.Minute >= set.Minute)
+            {
+                this.timer1.Enabled = false;
+                this.timer1.Stop();
+
+                try
+                {
+                    //启动本地程序并执行命令
+                    Process.Start("Shutdown.exe", " -s -t 0");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
 
        
--- trunk/STWebContainer/STWebContainerSetting.cs
+++ trunk/STWebContainer/STWebContainerSetting.cs
@@ -10,5 +10,12 @@ namespace STWebContainer
 
         public string Url="about:blank";
         public string Pwd="123456";
+        public int Hour=17;
+
+        public int Minute = 30;
+        
+        public bool IsTimer = false;
+        public bool IsShowToolBar=true;
+
     }
 }
