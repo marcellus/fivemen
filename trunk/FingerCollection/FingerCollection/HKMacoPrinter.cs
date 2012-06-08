c52c143b083f801a1f4f0cf459472f047bd8e4ee
deadshot123@gmail.com <deadshot123@gmail.com@862885c0-6149-0410-b969-135527a0a411>
2012-06-08 08:05:11 +0000

    新增打印一维条码和姓名的功能

--- trunk/FingerCollection/FingerCollection/HKMacoPrinter.cs
+++ trunk/FingerCollection/FingerCollection/HKMacoPrinter.cs
@@ -14,10 +14,12 @@ namespace FingerCollection
     {
 
         private string idcard;
+        private string name;
 
-        public HKMacoPrinter(string idcard)
+        public HKMacoPrinter(string idcard,string name)
         {
             this.idcard = idcard;
+            this.name = name;
         }
 
 
@@ -41,11 +43,13 @@ namespace FingerCollection
 
         public void PrintViewClient()
         {
+            SystemConfig config = FT.Commons.Cache.StaticCacheManager.GetConfig<SystemConfig>();
             Code39 code39 = new Code39();
             code39.WidthCU = 2;
             code39.LineHeight = 20;
             try
             {
+                this.Draw11String(name,new Point(config.LeftPoint+5,config.TopPoint));
                 SizeF titleSize = MyGraphics.MeasureString("1", code39.titleFont);
                 code39.Height = code39.topHeight + code39.LineHeight + (int)titleSize.Height;//定义图片高度          
                 Bitmap map = code39.CreateBarCode(this.idcard);
@@ -54,7 +58,7 @@ namespace FingerCollection
                 Image imagetest2 = Image.FromFile(path);
                 Image imagetmp2 = new System.Drawing.Bitmap(imagetest2);
 
-                MyGraphics.DrawImage(imagetmp2, new RectangleF(500 - 40, 300- 46, 240, 30));
+                MyGraphics.DrawImage(imagetmp2,config.LeftPoint, config.TopPoint+20);
                 imagetest2.Dispose();
                 File.Delete(path);
                 imagetmp2.Dispose();
