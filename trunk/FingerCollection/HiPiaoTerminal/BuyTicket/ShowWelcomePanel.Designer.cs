dcc225c9246a3abec37b13ad47d41e1ffe22932d
cbw123_1984@163.com <cbw123_1984@163.com@862885c0-6149-0410-b969-135527a0a411>
2012-11-06 12:52:47 +0000

    完成字体转换

--- trunk/FingerCollection/HiPiaoTerminal/BuyTicket/ShowWelcomePanel.Designer.cs
+++ trunk/FingerCollection/HiPiaoTerminal/BuyTicket/ShowWelcomePanel.Designer.cs
@@ -28,47 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbUserName = new FT.Windows.Controls.LabelEx.SimpleLabel();
-            this.label2 = new FT.Windows.Controls.LabelEx.SimpleLabel();
+            this.lbBalance = new FT.Windows.Controls.LabelEx.SimpleLabel();
             this.lbPoints = new FT.Windows.Controls.LabelEx.SimpleLabel();
             this.lbCoupons = new FT.Windows.Controls.LabelEx.SimpleLabel();
-            this.lbBalance = new FT.Windows.Controls.LabelEx.SimpleLabel();
+            this.label2 = new FT.Windows.Controls.LabelEx.SimpleLabel();
+            this.lbUserName = new FT.Windows.Controls.LabelEx.SimpleLabel();
             this.SuspendLayout();
             // 
-            // lbUserName
+            // lbBalance
             // 
-            this.lbUserName.AutoSize = true;
-            this.lbUserName.BackColor = System.Drawing.Color.Transparent;
-            this.lbUserName.Location = new System.Drawing.Point(119, 13);
-            this.lbUserName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
-            this.lbUserName.Name = "lbUserName";
-            this.lbUserName.Size = new System.Drawing.Size(78, 27);
-            this.lbUserName.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
-            this.lbUserName.TabIndex = 0;
-            this.lbUserName.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
-            this.label2.Location = new System.Drawing.Point(17, 13);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 27);
-            this.label2.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
-            this.label2.TabIndex = 1;
-            this.label2.Text = "欢迎回来";
+            this.lbBalance.AutoSize = true;
+            this.lbBalance.BackColor = System.Drawing.Color.Transparent;
+            this.lbBalance.Font = new System.Drawing.Font("方正兰亭纤黑简体", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbBalance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.lbBalance.Location = new System.Drawing.Point(302, 13);
+            this.lbBalance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbBalance.Name = "lbBalance";
+            this.lbBalance.Size = new System.Drawing.Size(189, 27);
+            this.lbBalance.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
+            this.lbBalance.TabIndex = 1;
+            this.lbBalance.Text = "账户余额：{0}元";
             // 
             // lbPoints
             // 
             this.lbPoints.AutoSize = true;
             this.lbPoints.BackColor = System.Drawing.Color.Transparent;
+            this.lbPoints.Font = new System.Drawing.Font("方正兰亭纤黑简体", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lbPoints.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
             this.lbPoints.Location = new System.Drawing.Point(302, 41);
             this.lbPoints.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbPoints.Name = "lbPoints";
-            this.lbPoints.Size = new System.Drawing.Size(111, 27);
+            this.lbPoints.Size = new System.Drawing.Size(117, 27);
             this.lbPoints.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
             this.lbPoints.TabIndex = 1;
             this.lbPoints.Text = "积分：{0}";
@@ -77,27 +67,42 @@
             // 
             this.lbCoupons.AutoSize = true;
             this.lbCoupons.BackColor = System.Drawing.Color.Transparent;
+            this.lbCoupons.Font = new System.Drawing.Font("方正兰亭纤黑简体", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lbCoupons.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
             this.lbCoupons.Location = new System.Drawing.Point(17, 41);
             this.lbCoupons.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbCoupons.Name = "lbCoupons";
-            this.lbCoupons.Size = new System.Drawing.Size(262, 27);
+            this.lbCoupons.Size = new System.Drawing.Size(277, 27);
             this.lbCoupons.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
             this.lbCoupons.TabIndex = 1;
             this.lbCoupons.Text = "兑换券：{0} 抵扣券：{1}";
             // 
-            // lbBalance
+            // label2
             // 
-            this.lbBalance.AutoSize = true;
-            this.lbBalance.BackColor = System.Drawing.Color.Transparent;
-            this.lbBalance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
-            this.lbBalance.Location = new System.Drawing.Point(302, 13);
-            this.lbBalance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lbBalance.Name = "lbBalance";
-            this.lbBalance.Size = new System.Drawing.Size(180, 27);
-            this.lbBalance.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
-            this.lbBalance.TabIndex = 1;
-            this.lbBalance.Text = "账户余额：{0}元";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("方正兰亭纤黑简体", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.label2.Location = new System.Drawing.Point(17, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 27);
+            this.label2.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
+            this.label2.TabIndex = 1;
+            this.label2.Text = "欢迎回来";
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.BackColor = System.Drawing.Color.Transparent;
+            this.lbUserName.Font = new System.Drawing.Font("方正兰亭纤黑简体", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbUserName.Location = new System.Drawing.Point(119, 13);
+            this.lbUserName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(82, 27);
+            this.lbUserName.Skin = FT.Windows.Controls.SimpleSkinType.Custom;
+            this.lbUserName.TabIndex = 0;
+            this.lbUserName.Text = "label1";
             // 
             // ShowWelcomePanel
             // 
